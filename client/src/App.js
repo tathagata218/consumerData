168c5bb2ab8bbba1ea4d8ae96ac7e9079b352929
tathagata218 <tathagata218@yahoo.com>
2018-01-23 22:42:18 -0600

    this is the stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header.js'
+import Stuff from './components/Header.js'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header message="This is the Header Tag" />
+        {/*<Header message="This is the Header Tag" />*/}
+        <h1> Thatagata Das </h1>
       </div>
     );
   }
